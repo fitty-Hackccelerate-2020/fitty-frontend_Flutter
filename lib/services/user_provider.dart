1122e0efcb333324975509fc0c56444a5c6e035a
Tirth7545 <tirthraj277@gmail.com>
2020-10-16 05:18:09 +0530

    addProvider and Shared prefrences

---
+++ lib/services/user_provider.dart
@@ -0,0 +1,14 @@
+import 'package:fitty/models/user.dart';
+import 'package:flutter/foundation.dart';
+
+
+class UserProvider with ChangeNotifier {
+  User _user = new User();
+
+  User get user => _user;
+
+  void setUser(User user) {
+    _user = user;
+    notifyListeners();
+  }
+}
